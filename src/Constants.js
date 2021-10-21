18c35c4f7271665d7a50f66ae85f2d07a644e1b0
jawad-claimmunity <jawad@claimmunity.com>
2021-10-21 14:59:04 -0500

    Changing to public hostname set up with route 53.

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://apps-external-lb-1937207210.us-east-2.elb.amazonaws.com/claimmunity'
+export const API_URL = 'https://lb.claimmunity.com/claimmunity'
 export const NUM_OF_RESULTS_PER_PAGE = 8;
